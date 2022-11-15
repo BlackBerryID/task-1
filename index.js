aac9489a8bd80b80427a04804fefa7b67a586d72
BlackBerryID <iddf1994one@gmail.com>
2022-11-15 23:27:35 +0300

    feat: add timer

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const inputEl = document.querySelector('input');
 const buttonEl = document.querySelector('button');
 const timerEl = document.querySelector('span');
+let timer;
 
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
 const createTimerAnimator = () => {
-  return (seconds) => {};
+
+  const displayTimeLeft = (seconds) => {    
+    const hours = Math.floor(seconds / 3600);
+    seconds = seconds % 3600;
+    const minutes = Math.floor(seconds / 60);
+    seconds = seconds % 60;
+
+    const timeToDisplay = `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : '' }${seconds}`;
+    timerEl.textContent = timeToDisplay;
+  }
+
+  return (seconds) => {
+    clearInterval(timer);
+
+    const now = Date.now();
+    const futureTime = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    
+    timer = setInterval(() => {
+      const secondsLeft = Math.round((futureTime - Date.now()) / 1000);
+
+      if(secondsLeft < 0) {
+        clearInterval(timer);
+        return;
+      }
+
+      displayTimeLeft(secondsLeft);
+    }, 1000)
+  };
 };
 
 const animateTimer = createTimerAnimator();
@@ -13,6 +42,8 @@ const animateTimer = createTimerAnimator();
 inputEl.addEventListener('input', (e) => {
   // Очистите input так, чтобы в значении
   // оставались только числа
+  const matches = e.target.value.match(/\d+/g);
+  e.target.value = matches ? e.target.value.match(/\d+/g).join('') : '';
 });
 
 buttonEl.addEventListener('click', () => {
